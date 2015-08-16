d317de13a7e936a9fc7b1093a111a8b8c97276b2
zwimeyihmh <zwimeyihmh@outlook.com>
2015-08-17 00:47:32 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
 var Controller = require('../controller/main.js');
 /* GET home page. */
 var controller = new Controller();
 router.get('/',controller.getManage);
+// router.post('/');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '123456',
+  database: 'test'
+});
+connection.connect();
+router.get("/del",function(req,res){
+  alert('del');
+  var deleteS = "delete from basic where name=" +'\''+req.query.nm +'\'';
+//  console.log(sql);
+  connection.query(deleteS,function(err,rows){
+    if(err) throw err;
+    //res.send("deleted");
+  });
+});
+connection.end();
 
 module.exports = router;
