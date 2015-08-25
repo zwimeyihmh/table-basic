80c4d411229d2f0b9e8c4d483ce73fc13098e13f
zwimeyihmh <zwimeyihmh@outlook.com>
2015-08-25 09:46:09 +0800

    add js

--- public/javascripts/sort.js
+++ public/javascripts/sort.js
@@ -1,9 +1,10 @@
 $(function() {
 
   $('thead').on('click', 'th', function() {
-    var skey = $(this).data('order');
+    var order = $(this).data('order');
     if ($(this).data('id') !== undefined) {
       var alls = [];
+      var _this = $(this);
       var subject = $(this).data('id');
 
       $('tbody tr').each(function(i, n) {
@@ -20,55 +21,28 @@ $(function() {
 
         });
       });
-
-      // console.log(subject);
-      // var sort = new Sort(alls,subject);
-      var result = [];
-      if (skey === 'or') {
-        if (subject === "chi") {
-          result = alls.sort(function(a, b) {
-            return parseInt(a.chinese) - parseInt(b.chinese);
-          });
-        }
-        if (subject === "mat") {
-          result = alls.sort(function(a, b) {
-            return parseInt(a.math) - parseInt(b.math);
-          });
-        }
-        if (subject === "eng") {
-          result = alls.sort(function(a, b) {
-            return parseInt(a.english) - parseInt(b.english);
-          });
-        }
-      }
-
-      if (skey === 'de') {
-        if (subject === "chi") {
-          result = alls.sort(function(a, b) {
-            return parseInt(b.chinese) - parseInt(a.chinese);
-          });
-        }
-        if (subject === "mat") {
-          result = alls.sort(function(a, b) {
-            return parseInt(b.math) - parseInt(a.math);
-          });
-        }
-        if (subject === "eng") {
-          result = alls.sort(function(a, b) {
-            return parseInt(b.english) - parseInt(a.english);
-          });
-        }
-      }
-      console.log(skey);
-      var k = 0;
-      $('tbody tr').each(function(i, n) {
-        var sc = $("td", this);
-        var person = sc.eq(0).text(result[k].name);
-        var chinese = sc.eq(1).text(result[k].chinese);
-        var math = sc.eq(2).text(result[k].math);
-        var english = sc.eq(3).text(result[k].english);
-        k++;
+      var result;
+      $.get('/score',{
+        alls:alls,
+        subject:subject,
+        order:order
+      },function(resq) {
+        // console.log(resq);
+        result=resq;
+        var k = 0;
+        $('tbody tr').each(function(i, n) {
+          var sc = $("td", this);
+            var person = sc.eq(0).text(result[k].name);
+            var chinese = sc.eq(1).text(result[k].chinese);
+            var math = sc.eq(2).text(result[k].math);
+            var english = sc.eq(3).text(result[k].english);
+            k++;
+        });
       });
+      console.log(result);
+
+      console.log(_this.data);
+      _this.data("order",-order);
     }
   });
 
@@ -80,30 +54,26 @@ $(function() {
     $('tbody tr').each(function(i, n) {
       var sc = $("td", this);
       var name = $("td", this).eq(0).text().trim();
-      var id = $("td",this).eq(0).data('id');
-      var person;
-      var chinese;
-      var math;
-      var english;
+      var id = $("td", this).eq(0).data('id');
       if (id === sid) {
         alert("删除？" + sid);
-        $(this).remove();
-        // $.ajax({
-        //   url:'/del',
-        //   data:sid,
-        //   type:'delete',
-        //   sucess:function() {}
-        // });
-
-        $.get("/del", {
-          id: sid
-        }, function(req) {
-
+        var self = $(this);
+        $.ajax({
+          url: '/del',
+          data: {
+            id: sid
+          },
+          type: 'delete',
+          success: function(information) {
+            console.log(information);
+            if (information.status === 200) {
+              self.remove();
+            }
+          }
         });
       }
 
     });
-    // alert(alls);
   });
 
   $('.add').on('click', function() {
@@ -111,18 +81,23 @@ $(function() {
     var addc = prompt("chines=?");
     var addm = prompt("math=?");
     var adde = prompt("english=?");
-    var newRow = "<tr><td>" + addn + "</td><td>" + addc + "</td><td>" + addm + "</td><td>" + adde + "</td>" + " <td><button type='button' name='button' class='delete' id='del'>删除</button></td><tr>";
-    $("tbody").append(newRow);
     var add = {
       name: addn,
       chinese: parseInt(addc),
       math: parseInt(addm),
       english: parseInt(adde)
     };
-    $.get("/add", {
-      added: add
-    }, function(req) {
-      // alert('sort');
+    $.ajax({
+      url: '/add',
+      data: add,
+      type: 'post',
+      success: function(information) {
+        if (information.status === 200) {
+          var newRow = "<tr><td data-id='" +information.data +"'>"+ addn + "</td><td>" + addc + "</td><td>" + addm + "</td><td>" + adde + "</td>" + " <td><button type='button' name='button' class='delete' id='del'>删除</button></td><tr>";
+          $("tbody").append(newRow);
+
+        }
+      }
     });
   });
 
